bff4443dcce93bb184b000123c10fa3d8dab1d76
Mohd Abdul Aleem <mohammedaleem061@gmail.com>
2024-01-14 01:07:38 +0530

    Background change

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,9 +20,9 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/my-photo.jpeg";
+import image from "../images/coffee.jpg";
 
-const imageAltText = "My photo. This is the background";
+const imageAltText = "This is the background";
 
 const Home = ({ name, title }) => {
   return (
